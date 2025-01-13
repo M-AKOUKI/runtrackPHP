30fb6e0eaa5e46b309794433cb4b5f2d27ecc8fd
M-AKOUKI <mohamed-amine.kouki@laplateforme.io>
2025-01-13 11:41:47 +0100

    feat:ajout de fichier

---
+++ Jour1/Job1/index.php
@@ -0,0 +1,13 @@
+<?php
+
+echo"Hello La Plateforme";
+$str = "Laplateforme";
+$str2 = "Vive";
+$str3 = "!";
+$val = 6;
+$myBool = True;
+$myBool = False;
+echo $str2," ",$str," ",$str3;
+echo $val;
+echo $val + 4;
+echo $myBool;
